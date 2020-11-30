8cbde04fb055b107e547c4600070b55d440777a5
Monich <Monich17v@gmail.com>
2020-11-30 17:21:34 +0200

    Update

--- index.php
+++ index.php
@@ -1,18 +1,22 @@
 <?php
 $name="Vitaliy";
-$a = array ("Привет", "Мир!", "Меня", "зовут", $name);
-print_r($a);
+$a = ["Привет", "Мир!", "Меня", "зовут", $name];
+echo($a[0]." ".$a[1]." ".$a[2]." ".$a[3]." ".$a[4]." | ");
+
 
 $arr = [
-    "dep1" => array("Kolya" => 1000, "Oleg" => 700, "Olya" => 1100),
-    "dep2" => array("Nik" => 900, "Jhon" => 800, "Boss" => 1600),
+    "dep1" => ["Kolya" => 1000, "Oleg" => 700, "Olya" => 1100],
+    "dep2" => ["Nik" => 900, "Jhon" => 800, "Boss" => 1600],
 ];
 
-echo('Dep1 Salary: '.array_sum($arr["dep1"]));
-echo('Dep2 Salary: '.array_sum($arr["dep2"]));
+echo('Dep1 Salary: '.array_sum($arr["dep1"])." | ");
+echo('Dep2 Salary: '.array_sum($arr["dep2"])." | ");
 
-$array = array(1, 2, 3, 4, 5, 6);
-print_r(array_slice($array, 3));
-print_r(array_splice($array, 0, 1));
-print_r(array_splice($array, -1));
-print_r($array);
+$array = [1, 2, 3, 4, 5, 6];
+$r1=array_slice($array, 3);
+print_r($r1);
+echo " | ";
+print_r(array_splice($r1, 0, 1));
+print_r(array_splice($r1, -1));
+echo " | ";
+print_r($r1);
