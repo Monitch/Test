3f1de076bd64ac2c2c010a55c134db9a740c0377
Monich <Monich17v@gmail.com>
2020-12-24 13:48:10 +0200

    psr-12_Fix

--- CatClass.php
+++ CatClass.php
@@ -1,58 +1,66 @@
 <?php
 class Cat{
-    private int $Age;
-    private string $Color;
-    private string $Breed;
-    private string $Sex;
-    private int $Size;
+    private int $age;
+    private string $color;
+    private string $breed;
+    private string $boyOrGirl;
+    private int $size;
 
     public function __construct(
-        int $Age,
-        string $Color,
-        string $Breed,
-        string $Sex,
-        int $Size
+        int $age,
+        string $color,
+        string $breed,
+        string $boyOrGirl,
+        int $size
     ){
-        $this->Age =$Age;
-        $this->Color=$Color;
-        $this->Breed=$Breed;
-        $this->Sex=$Sex;
-        $this->Size=$Size;
+        $this->age =$age;
+        $this->color=$color;
+        $this->breed=$breed;
+        $this->boyOrGirl=$boyOrGirl;
+        $this->size=$size;
 
     }
-
 
     public function isBoy(): bool
     {
-        return $this->Sex=="Man";
+        return $this->boyOrGirl == "Man";
     }
     public function isGirl(): bool
     {
-        return $this->Sex=="Women";
+        return $this->boyOrGirl == "Women";
     }
-    public function isPerfrectCat() :bool{
+
+    public function isPerfrectCat(): bool
+    {
         return (
             $this->isPerfrectAge() &&
             $this->isPerfrectBreed() &&
             $this->isPerfrectSize() &&
             $this->isPerfrectColor()
         );
-
-    }
-    private function isPerfrectAge() :bool{
-        return $this->Age < 7 && $this->Age > 2;
-    }
-    private function isPerfrectColor() :bool{
-        return $this->Color == "Grey";
-    }
-    private function isPerfrectBreed() :bool{
-        return $this->Breed == "Street";
-    }
-    private function isPerfrectSize() :bool{
-        return $this->Size == 6;
     }
 
+    private function isPerfrectAge(): bool
+    {
+        return $this->age < 7 && $this->age > 2;
+    }
+
+    private function isPerfrectColor(): bool
+    {
+        return $this->color == "Grey";
+    }
+
+    private function isPerfrectBreed(): bool
+    {
+        return $this->breed == "Street";
+    }
+
+    private function isPerfrectSize(): bool
+    {
+        return $this->size == 6;
+    }
 }
+
  $Poenix=new Cat(3,"Grey","Street","Man",6);
  var_dump($Poenix->isBoy());
  var_dump($Poenix->isPerfrectCat());
--- Repead.php
+++ Repead.php
@@ -1,15 +1,27 @@
-
 <?php
 session_start();
-$name = "Vitaliy";
+header("Content-Type: text/plain; charset=utf-8");
 
+
+if (!isset($_SESSION['time'])&& !isset($_SESSION['$age'])) {
+    require_once("htmltest.html");
+    $_SESSION['$name'] = $_POST['name'];
+    $_SESSION['$age'] = $_POST['age'];
+    echo $_SESSION['$name'] . " " . $_SESSION['$age'];
+} else {
+    echo $_SESSION['$name']." ".$_SESSION['$age'];
+}
+
+echo PHP_EOL;
+
+$name = "Vitaliy";
 $superName = "Monich";
 $array = [];
 $array = addValue($array, $name, $superName);
 print_r($array);
 
-function addValue(array $arr,$name,$superName){
-
+function addValue(array $arr,$name,$superName)
+{
     $arr[] = $name;
     $arr[] = $superName;
     return $arr;
@@ -18,7 +30,8 @@ function addValue(array $arr,$name,$superName){
 $emailProvider="GMAIL";   // GMAIL, MAILRU, YANDEX
 getEmail($emailProvider);
 
-function getEmail($email){
+function getEmail($email)
+{
     switch ($email) {
         case "GMAIL":
             sendGmail();
@@ -32,24 +45,17 @@ function getEmail($email){
     }
 }
 
-function sendGmail(){
+function sendGmail()
+{
     echo "It's Gmail";
 }
 
-function sendMailRu(){
+function sendMailRu()
+{
     echo "It's MailRu";
 }
 
-function sendYandex(){
+function sendYandex()
+{
     echo "It's Yandex";
 }
-
-if (!isset($_SESSION['time'])&& !isset($_SESSION['$age'])){
-    require_once("htmltest.html");
-$_SESSION['$name'] = $_POST['name'];
-$_SESSION['$age'] = $_POST['age'];
-echo $_SESSION['$name']." ".$_SESSION['$age'];}
-else{
-    echo "g";
-echo $_SESSION['$name']." ".$_SESSION['$age'];}
-?>
